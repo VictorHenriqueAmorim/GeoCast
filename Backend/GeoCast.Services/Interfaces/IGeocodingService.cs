badc116bae5726804da1f4c331b05a98a977645d
Victor Amorim <victoramorim895623@gmail.com>
2024-01-12 20:25:26 -0300

    feat(interfaces): add geocoding service interface

---
+++ Backend/GeoCast.Services/Interfaces/IGeocodingService.cs
@@ -0,0 +1,10 @@
+﻿using GeoCast.Services.Models.Geocoding;
+
+namespace GeoCast.Services.Interfaces
+{
+    public interface IGeocodingService
+    {
+        Task<GeocodingResult> GeocodeAddressAsync(string address);
+    }
+
+}
