3b2b394581759ee3532531c341ba8b60179a85cf
Victor Amorim <victoramorim895623@gmail.com>
2024-01-14 20:16:32 -0300

    chore(services): change hardcoded url to IConfiguration url

--- Backend/GeoCast.Services/Services/GeocodingService.cs
+++ Backend/GeoCast.Services/Services/GeocodingService.cs
@@ -1,26 +1,24 @@
 ﻿using GeoCast.Services.Interfaces;
 using GeoCast.Services.Models.Geocoding;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.Extensions.Configuration;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace GeoCast.Services.Services
 {
     public class GeocodingService : IGeocodingService
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public GeocodingService(HttpClient httpClient)
+        public GeocodingService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
 
         public async Task<List<AddressMatch>?> GeocodeAddressAsync(string address)
         {
-            string url = $"https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={Uri.EscapeDataString(address)}&benchmark=2020&format=json";
+            string url = $"{_configuration["GeocodingAPI"]}/onelineaddress?address={Uri.EscapeDataString(address)}&benchmark=2020&format=json";
 
             var response = await _httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
