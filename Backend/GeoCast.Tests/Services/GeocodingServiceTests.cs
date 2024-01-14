8c4aa32add3cecf9193864a3f540d2254a14b616
Victor Amorim <victoramorim895623@gmail.com>
2024-01-14 16:04:05 -0300

    chore(tests): fix models and remove GeocodeAddressAsync_Should_Fail integration test

--- Backend/GeoCast.Tests/Services/GeocodingServiceTests.cs
+++ Backend/GeoCast.Tests/Services/GeocodingServiceTests.cs
@@ -7,7 +7,7 @@ namespace GeoCast.Tests.Services
     public class GeocodingServiceTests
     {
         private readonly Mock<IGeocodingService> _mockGeocodingService;
-        private readonly IGeocodingService _geocodingService;
+
         public GeocodingServiceTests()
         {
             _mockGeocodingService = new Mock<IGeocodingService>();
@@ -19,29 +19,16 @@ namespace GeoCast.Tests.Services
 
             // Arrange
             var validAddress = "4600 Silver Hill Rd, Washington, DC 20233";
-            var mockCoordinate = new Coordinate { Latitude = 38.846016, Longitude = -76.927487 };
-            var expectedCoordinate = new GeocodingResult();
-            expectedCoordinate.Coordinates.Add(mockCoordinate);
+            var mockCoordinate = new Coordinates { Latitude = 38.846016, Longitude = -76.927487 };
+            var addressMatches = new List<AddressMatch> { new AddressMatch() { Coordinates = mockCoordinate, MatchedAddress = validAddress } };
             _mockGeocodingService.Setup(service => service.GeocodeAddressAsync(validAddress))
-                                 .ReturnsAsync(expectedCoordinate);
+                                 .ReturnsAsync(addressMatches);
 
             // Act
             var result = await _mockGeocodingService.Object.GeocodeAddressAsync(validAddress);
 
             // Assert
-            Assert.Equal(expectedCoordinate, result);
+            Assert.Equal(addressMatches, result);
         }
-
-        [Fact]
-        public async Task GeocodeAddressAsync_Should_Fail()
-        {
-            // Arrange
-            var invalidAddress = "jsnadidpf";
-
-            // Act & Assert
-            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _geocodingService.GeocodeAddressAsync(invalidAddress));
-            Assert.Equal("Invalid Address", exception.Message);
-        }
-
     }
 }
