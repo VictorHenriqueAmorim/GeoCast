489385dab45df4e4f1a97711c2e94293c100f435
Victor Amorim <victoramorim895623@gmail.com>
2024-01-14 15:59:39 -0300

    chore(models): change geocode structure

---
+++ Backend/GeoCast.Services/Models/Geocoding/Geocode.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GeoCast.Services.Models.Geocoding
+{
+    public class Geocode
+    {
+
+        [JsonPropertyName("addressMatches")]
+        public List<AddressMatch> AddressMatches { get; set; }
+    }
+}
--- Backend/GeoCast.Services/Models/Geocoding/GeocodingResult.cs
+++ Backend/GeoCast.Services/Models/Geocoding/GeocodingResult.cs
@@ -4,7 +4,7 @@ namespace GeoCast.Services.Models.Geocoding
 {
     public class GeocodingResult
     {
-        [JsonPropertyName("addressMatches")]
-        public List<Coordinate> Coordinates { get; set; }
+        [JsonPropertyName("result")]
+        public Geocode Geocode { get; set; }
     }
 }
