bdf1788ba627d2e58b8be0e6a824b44d25d1a431
Victor Amorim <victoramorim895623@gmail.com>
2024-01-15 12:34:53 -0300

    feat(components): add Weather card

---
+++ Frontend/geocast/src/components/Weather/WeatherCard/styled.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+import { Card, CardContent } from "@mui/material";
+
+const CardStyle = styled(Card)`
+  width: 100%;
+`;
+
+const CardContentStyle = styled(CardContent)`
+    text-align: center;
+`
+export {
+    CardStyle,
+    CardContentStyle
+}
